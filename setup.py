793decf8a4350d9d4fbd90f641d65d95562ffdf0
nickmetal <nickmetal92@gmail.com>
2017-04-16 15:10:31 +0300

    Prepared for Pypi

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     url='https://github.com/nickmetal/FileLogger',
     description='Wrapper class for RotatingFileHandler from logging module',
     name='filelogger',
-    version='0.31',
+    version='0.35',
     packages=['filelogger'],
-    author='Norroen',
+    author='Me',
     author_email='nickmetal92@gmail.com',
-    download_url='https://github.com/nickmetal/filelogger/archive/0.31.tar.gz',
+    download_url='https://github.com/nickmetal/filelogger/archive/0.35.tar.gz',
 )
